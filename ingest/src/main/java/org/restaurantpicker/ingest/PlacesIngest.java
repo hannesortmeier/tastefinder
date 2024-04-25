f47bc7ea47fc41a8188084fb9dc90df19fd82cb1
Hannes Ortmeier <ortmeier.hannes@gmail.com>
2024-04-25 22:40:10 +0200

    fix: Pass region to s3 client builder

--- ingest/src/main/java/org/restaurantpicker/ingest/PlacesIngest.java
+++ ingest/src/main/java/org/restaurantpicker/ingest/PlacesIngest.java
@@ -7,6 +7,7 @@ import java.nio.charset.StandardCharsets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 public class PlacesIngest {
@@ -17,12 +18,12 @@ public class PlacesIngest {
 
     final var apiKey = System.getenv("MAPS_API_KEY");
     if (apiKey == null) {
-      throw new RuntimeException("Environment variable MAP_API_KEY not set");
+      throw new RuntimeException("Environment variable MAPs_API_KEY not set");
     }
 
     try (final var placesClient = new PlacesClient(apiKey)) {
       final var callback = new PlacesSearchCallback();
-      try (var in = PlacesIngest.class.getResourceAsStream("/plz_berlin.txt")) {
+      try (var in = PlacesIngest.class.getResourceAsStream("/plz_frankfurt.txt")) {
         final var reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
         for (String line; (line = reader.readLine()) != null; ) {
           var query = "Restaurant " + line;
@@ -34,6 +35,7 @@ public class PlacesIngest {
       try (final var client =
           S3Client.builder()
               .credentialsProvider(ProfileCredentialsProvider.create(credentialsProfile))
+              .region(Region.EU_CENTRAL_1)
               .build()) {
         final var coordinatesToResults = callback.getCoordinatesToResults();
         final var s3Handler = new S3Handler(client);
